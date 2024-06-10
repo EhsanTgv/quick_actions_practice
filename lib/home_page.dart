e11f105e495fae54cd100ec88a9b277f5fb092ab
EhsanTgv <EhsanTgv@gmail.com>
2024-06-10 11:33:55 +0300

    Implement Quick actions

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quick_actions/quick_actions.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -10,6 +11,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  QuickActions quickActions = const QuickActions();
+
+  @override
+  void initState() {
+    initializeQuickActions();
+    super.initState();
+  }
+
+  initializeQuickActions() {
+    quickActions.initialize((String shortcutType) {
+      switch (shortcutType) {
+        case 'First Page Screen':
+          _navigate('/firstpage');
+          return;
+        case 'Second Page Screen':
+          _navigate('/secondpage');
+          return;
+        default:
+          _navigate('/firstpage');
+          return;
+      }
+    });
+
+    quickActions.setShortcutItems(<ShortcutItem>[
+      const ShortcutItem(
+        type: 'First Page Screen',
+        localizedTitle: 'First Page',
+      ),
+      const ShortcutItem(
+        type: 'Second Page Screen',
+        localizedTitle: 'Second Page',
+      ),
+    ]);
+  }
+
+  _navigate(String screen) {
+    Navigator.of(context)
+        .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+    Navigator.pushNamed(context, screen);
+  }
 
   @override
   Widget build(BuildContext context) {
